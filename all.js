a28de109f1668fa0d6e5555b1ca11768396aebe3
John Paul <johnpaulustaris@gmail.com>
2024-08-12 19:57:09 +0800

    Add data.json for data

--- all.js
+++ all.js
@@ -1,3 +1,4 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
+
+    document.getElementById('theme-toggle').addEventListener('click', function() {
     document.body.classList.toggle('dark');
 });
